8a9349e22d02a4a3005744ad5df882920762b16c
Zahidul Haque <iisowrovii@gmail.com>
2016-09-04 23:27:39 +0600

    add send valiable into view template

--- system/stupid_controller.php
+++ system/stupid_controller.php
@@ -1,12 +1,19 @@
 <?php
 class StupidController{
+    public $view = array();
+
     public function execute($action){
         $this->$action();
         $this->render($action);
     }
 
     public function render($action){
+        $view = (object) $this->view;
         require_once $this->getView($action);
+    }
+
+    public function view($name, $value){
+        $this->view[$name] = $value;
     }
 
     private function getView($action){
